758e07f64128a10ea94537232326d062a6b7d7a9
abir <izatop@gmail.com>
2021-05-11 16:50:57 +0300

    fix: the numeric input type added

---
+++ packages/input/src/Type/Numeric.ts
@@ -0,0 +1,18 @@
+import {isNumber, isString} from "@bunt/util";
+import {ScalarType} from "./ScalarType";
+
+export const Numeric = new ScalarType<number, string | number>({
+    name: "Numeric",
+    validate(payload) {
+        this.assert(isNumber(payload) || isString(payload), `Wrong payload: ${this.name} expected`, payload);
+        const value = isNumber(payload) ? payload : parseInt(payload, 10);
+
+        this.assert(
+            !isNaN(value) && Number.isSafeInteger(value),
+            `Wrong payload: ${this.name} expected`,
+            payload,
+        );
+
+        return value;
+    },
+});
