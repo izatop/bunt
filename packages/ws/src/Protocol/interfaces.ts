99b25feb009a0641873a718b83a916aac93fd39f
abir <izatop@gmail.com>
2021-05-28 11:39:02 +0300

    fix: types

--- packages/ws/src/Protocol/interfaces.ts
+++ packages/ws/src/Protocol/interfaces.ts
@@ -1,7 +1,7 @@
-import {Context} from "@bunt/unit";
+import {ApplyContext, Context} from "@bunt/unit";
 import {ProtoHandleAbstract} from "./index";
 
 export type HandleProtoType<C extends Context, A extends ProtoHandleAbstract<C, any>> = {
-    new(context: C, state: any): A;
+    new(context: ApplyContext<C>, state: any): A;
     isSupported(protocol: string): boolean
 };
