de2830eb9727a6270b7822773313532b6486cede
abir <izatop@gmail.com>
2022-07-20 17:28:16 +0300

    fix: context

--- packages/unit/src/Context/interfaces.ts
+++ packages/unit/src/Context/interfaces.ts
@@ -2,8 +2,8 @@ import {IServiceResolver} from "../interfaces";
 
 export type ResolveService<T> = T extends IServiceResolver<infer S> ? Promise<S> : T;
 
-export type ApplyContext<C> = {
+export type ApplyContext<C> = C & {
     [K in keyof C]: C[K] extends IServiceResolver<infer S>
-        ? S extends Promise<infer R> ? R : C[K]
+        ? S
         : C[K];
 };
