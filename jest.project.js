c65196ff75f3a66a219ce7a5982f927d55daa07f
abir <izatop@gmail.com>
2023-05-30 12:56:52 +0400

    fix: tests

--- jest.project.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    transform: {"src/.+\\.[jt]s?$": ["ts-jest", {}]},
-    moduleNameMapper: {
-        "^([\\.]+\\/.+)\\/index\\.js": "$1",
-        "^([\\.]+\\/.+)\\.js": "$1",
-    },
-    rootDir: "src",
-};
